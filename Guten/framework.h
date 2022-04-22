1d1fb71c2381a184b2f54ff32ee3b71edb1b3eed
Kevin Chahine <kchahine17@gmail.com>
2022-04-22 22:08:32 +0100

    Small change to framework.h

--- Guten/framework.h
+++ Guten/framework.h
@@ -7,7 +7,11 @@
 	
 #elif _WIN32
 	#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+	#ifndef NOMINMAX
+		#define NOMINMAX   
+	#endif
+	
 	// Windows Header Files
-	#define NOMINMAX   
 	#include <windows.h>
 #endif // _WIN32
