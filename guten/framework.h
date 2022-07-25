dcb73ba972f5ef463f28aa8ef443bf7c2abb1e19
Kevin Chahine <kchahine17@gmail.com>
2022-07-25 14:11:51 -0400

    Small change to framework.h

--- guten/framework.h
+++ guten/framework.h
@@ -6,8 +6,10 @@
 	using CONSOLE_SCREEN_BUFFER_INFO = void *;	// TODO: Fix this for linux
 	
 #elif _WIN32
-	#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-
+	#ifndef WIN32_LEAN_AND_MEAN
+		#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+	#endif // WIN32_LEAN_AND_MEAN
+	
 	#ifndef NOMINMAX
 		#define NOMINMAX   
 	#endif
